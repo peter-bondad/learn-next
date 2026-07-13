5b5c56afbec3f60e5be228aad869f851008c93e9
Peter Bondad <bondadpetermaironne@gmail.com>
2026-07-13 17:08:49 +0800

    refactor(utils): remove unused functions

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,15 +1,3 @@
-export function utcNow(): Date {
-  return new Date();
-}
-
-export function utcHoursFromNow(hours: number): Date {
-  return new Date(Date.now() + hours * 60 * 60 * 1000);
-}
-
-export function utcMinutesFromNow(minutes: number): Date {
-  return new Date(Date.now() + minutes * 60 * 1000);
-}
-
 export function isExpired(date: Date | null | undefined): boolean {
   if (!date) return false;
 
