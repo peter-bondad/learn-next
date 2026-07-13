10615eef230d58a8709f34d4d5d392f0dd699988
Peter Bondad <bondadpetermaironne@gmail.com>
2026-07-13 17:05:11 +0800

    refactor(env): remove next app url

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -13,7 +13,6 @@ export const env = createEnv({
   client: {
     NEXT_PUBLIC_APP_NAME: z.string().min(2).max(100),
     NEXT_PUBLIC_APP_DESCRIPTION: z.string().max(256),
-    NEXT_PUBLIC_APP_URL: z.string(),
   },
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
@@ -24,6 +23,5 @@ export const env = createEnv({
     RESEND_API_KEY: process.env.RESEND_API_KEY,
     NEXT_PUBLIC_APP_NAME: process.env.NEXT_PUBLIC_APP_NAME,
     NEXT_PUBLIC_APP_DESCRIPTION: process.env.NEXT_PUBLIC_APP_DESCRIPTION,
-    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
   },
 });
