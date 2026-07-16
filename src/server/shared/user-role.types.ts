f20f36767ecd942f1992769a8f8c0008bdbaadf1
Peter Bondad <bondadpetermaironne@gmail.com>
2026-07-16 18:02:21 +0800

    refactor(user): make user role type checking type-safe

--- src/server/shared/user-role.types.ts
+++ src/server/shared/user-role.types.ts
@@ -4,7 +4,17 @@ export const userRole = {
   Staff: "staff",
 } as const;
 
+// access to the value of each key of userrole
 export type UserRole = (typeof userRole)[keyof typeof userRole];
+
+// access to key of userRole object
+export type UserString = keyof typeof userRole;
+
+export const userRoleLabels: Record<UserRole, UserString> = {
+  owner: "Owner",
+  manager: "Manager",
+  staff: "Staff",
+};
 
 export function isUserRole(role: unknown): role is UserRole {
   return Object.values(userRole).includes(role as UserRole);
